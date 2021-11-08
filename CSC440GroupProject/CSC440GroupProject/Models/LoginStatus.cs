6630fe5d8ec89cc91a44dbd76e5f486071f0f8f8
Jadon Calvert <calvertjadon@gmail.com>
2021-11-07 23:30:43 -0500

    Remove INotify from model

--- CSC440GroupProject/CSC440GroupProject/Models/LoginStatus.cs
+++ CSC440GroupProject/CSC440GroupProject/Models/LoginStatus.cs
@@ -8,21 +8,9 @@ using System.Threading.Tasks;
 
 namespace CSC440GroupProject.Models
 {
-    class LoginStatus : ViewModelBase
+    class LoginStatus 
     {
-        private string status;
-        public string Status
-        {
-            get
-            {
-                return status;
-            }
-            set
-            {
-                this.status = value;
-                OnPropertyChanged(nameof(Status));
-            }
-        }
+        public string Status;
 
         public LoginStatus(string status)
         {
