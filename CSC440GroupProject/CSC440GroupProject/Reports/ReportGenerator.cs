c7cd9cf8b65a9c2d7a9cd68a8da044ddfcd9858f
Jadon Calvert <35844995+calvertjadon@users.noreply.github.com>
2021-11-18 15:34:11 -0500

    Begin report generation implementation

---
+++ CSC440GroupProject/CSC440GroupProject/Reports/ReportGenerator.cs
@@ -0,0 +1,23 @@
+﻿using CSC440GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC440GroupProject.Reports
+{
+    abstract class ReportGenerator
+    {
+        public Student SelectedStudent { get; private set; }
+        public List<Grade> Grades { get; private set; }
+
+        protected ReportGenerator(Student selectedStudent, List<Grade> grades)
+        {
+            SelectedStudent = selectedStudent;
+            Grades = grades;
+        }
+
+        public abstract void GenerateReport();
+    }
+}
--- CSC440GroupProject/CSC440GroupProject/ViewModels/ImportRecordsViewModel.cs
+++ CSC440GroupProject/CSC440GroupProject/ViewModels/ImportRecordsViewModel.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using CSC440GroupProject.Reports;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CSC440GroupProject.ViewModels
 {
     class ImportRecordsViewModel
     {
+        public ICommand GenerateReportCommand { get; set; }
+
+        public ImportRecordsViewModel()
+        {
+            GenerateReportCommand = new BaseCommand(GeneratePlainTextReport);
+        }
+
+        private void GeneratePlainTextReport(object _)
+        {
+            ReportGenerator reportGenerator = new PlainTextReportGenerator(
+                new Models.Student(),
+                null
+            );
+            reportGenerator.GenerateReport();
+        }
     }
 }
